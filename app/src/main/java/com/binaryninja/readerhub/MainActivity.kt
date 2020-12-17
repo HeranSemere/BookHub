e9f01014a20c573840b4510538a46c4edc84de97
outThabox <heranseme@gmail.com>
2020-12-17 13:50:08 +0300

    Added icons on toolbar

--- app/src/main/java/com/binaryninja/readerhub/MainActivity.kt
+++ app/src/main/java/com/binaryninja/readerhub/MainActivity.kt
@@ -1,18 +1,30 @@
 package com.binaryninja.readerhub
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       /* val actionBar: ActionBar? = supportActionBar
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setLogo(R.drawable.search)
+        supportActionBar?.setLogo(R.drawable.vert)
+        supportActionBar?.setDisplayUseLogoEnabled(true)*/
+
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
@@ -23,4 +35,29 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+  /*  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(onOptionsItemSelected(item)){
+            return true
+        }
+
+        val id = item.itemId
+
+        if (id == R.id.search_icon) {
+            Toast.makeText(this, "Item One Clicked", Toast.LENGTH_LONG).show()
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }*/
+
+
 }
