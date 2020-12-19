41ea14238ac2e5283e57ccc5edb7995811e21cdf
outThabox <heranseme@gmail.com>
2020-12-19 15:52:03 +0300

    Request dialoge added

--- app/src/main/java/com/binaryninja/readerhub/ui/mybooks/BookDetail.kt
+++ app/src/main/java/com/binaryninja/readerhub/ui/mybooks/BookDetail.kt
@@ -1,12 +1,16 @@
 package com.binaryninja.readerhub.ui.mybooks
 
+import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.core.content.ContextCompat
 import com.binaryninja.readerhub.OwnersBooks
 import com.binaryninja.readerhub.R
 import kotlinx.android.synthetic.main.activity_book_detail.*
+import kotlinx.android.synthetic.main.filter_dialoge.view.*
+import kotlinx.android.synthetic.main.request_dialoge.view.*
 
 class BookDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +24,24 @@ class BookDetail : AppCompatActivity() {
         ownerProfile.setOnClickListener {
             val intent = Intent(this, OwnersBooks::class.java)
             ContextCompat.startActivity(this, intent, null)
+        }
+
+        requestAlert.setOnClickListener {
+            val filterDialogView= LayoutInflater.from(this).inflate(R.layout.request_dialoge,null)
+
+            val mBuilder= AlertDialog.Builder(this)
+                .setView(filterDialogView)
+
+            val mAlertDialog=mBuilder.show()
+
+            filterDialogView.dialogeRequestBackImage.setOnClickListener {
+
+                mAlertDialog.dismiss()
+            }
+
+            filterDialogView.requestButton.setOnClickListener {
+                mAlertDialog.dismiss()
+            }
         }
     }
 }
